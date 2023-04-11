7f57b34b1734ff30aa72968570abaa1b6a924ebd
andyjusa <90822680+andyjusa@users.noreply.github.com>
2023-04-11 21:10:55 +0900

    initalize

---
+++ 1st.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*  "1 1 1 1.00" 출력  
+    기본적으로 입력되어 있는건 건들지 말자 */
+int main()
+{
+    int a;
+    char b;
+    long c;
+    float d;
+
+    
+
+    printf("%d %c %ld %f",a,b,c,d);
+    return 0;
+}
---
+++ 2nd.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/*    for 문을 사용하여 1부터 10까지 출력하기    */
+int main()
+{
+
+}
